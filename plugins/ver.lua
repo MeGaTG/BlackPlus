ebe9d101d8b9fa2e1bea24c91191391996d32979
This_Is_Amir <arabiamir13@gmail.com>
2016-05-24 05:57:22 -0700

    XD :D

--- plugins/ver.lua
+++ plugins/ver.lua
@@ -1,32 +1,26 @@
 do
 
 function run(msg, matches)
-  return [[ TELE 🔶PHOENIX🔶
-=================================
-A new bot for manage your Supergroups.
-
-=================================
-
- #Developer : @Tis_Is_Amir(Amir)
-
-=================================
-
-#Manager : @ALIREZASOLTANI1376(Ali)
-
-================================
-
-Thanks To :
-Mehsi-HS
-ShahabDark
-================================
-Bot version : 1.2 ]]
+  local text = [[ [OffliNeTG](http://telegram.me/OffliNeTG)
+  
+*Version: 1.1*
+  
+_more information send 
+!OffliNeTG 
+to chat_
+[#Dev](http://telegram.me/this_Is_Amir)
+[Join Us :D](http://telegram.me/OffliNeTeam)
+*TNX to all*]]
+send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
-
 return {
   description = "Shows bot version", 
-  usage = "version: Shows bot version",
+  usage = "!version: Shows bot version",
   patterns = {
-    "^[#!/]version$"
+    "^[#!/]version$",
+    "^[#!/]ver$",
+    "^[Vv]ersion$",
+    "^[Vv]er$"
   }, 
   run = run 
 }
