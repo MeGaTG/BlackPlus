3bef7fdfa3a386ef37c3f69252fecbfd00053350
This_Is_Amir <arabiamir13@gmail.com>
2016-05-25 15:35:31 -0700

    XD :D

--- plugins/ofline.lua
+++ plugins/ofline.lua
@@ -1,39 +1,21 @@
-do
+local function run(msg)
+local text = [[*OFFLINETG Ver :* 2.1
+_An Administrator The PowerFull Bot Based On TeleSeed And Created With_ [OFFLINETEAM](telegram.me/offlineteam)
 
-local function run(msg, matches)
-  if matches[1]:lower() == 'OffLiNeTG' or 'OFFLINETG' or 'offlinetg' or 'OFFLINETEAM' then --change this with anything you want
-    send_document(get_receiver(msg), "./axs/telebeyond.gif", ok_cb, false) --put here sticker location(if you want)
-      return [[ 
-  
-*OFFLINETG Ver :* `2.1`
-_An Administrator The PowerFull Bot Based On TeleSeed And Bilingual Written In Lua_
-    
 Sudo Users :
-`Developer&Founder` : [AMIR](telegram.me/this_is_amir)
-`Developer&Manager` : [AliReZa](telegram.me/Mr_Clown_Developer)
+
+Developer&Founder : [AMIR](telegram.me/this_is_amir)
+Developer&Manager : [AliReZa](telegram.me/Mr_Clown_Developer)
+
 *Team Channel :*
 [Join Us :D](telegram.me/offlineteam)
-     
+
 *Special Thx To :*
 _ShahabDark_
 _SEEDTEAM_
-_And All My Friends :D_
-     
- ]]
-  end
+_And All My Friends :D_]]
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
-
 return {
-  patterns = {
-    "^[!/#](OFFLINETG)$",
-    "^(OFFLINETG)$",
-    "^[!/#]([offlinetg)$",
-    "^([offlinetg)$",
-    "^(OffLiNeTG)$",
-    "^[!/#]([OffLiNeTG)$",
-    "^(OFFLINETEAM)$",
-    "^[!/#](OFFLINETEAM)$"
-    },
-  run = run
-}
-end
+ patterns = {"^!OffLiNeTG$",},
+ run = run }
