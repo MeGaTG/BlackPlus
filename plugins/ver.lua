ce6690aa1ac02eca378d86f8f8246d13612bd939
This_Is_Amir <arabiamir13@gmail.com>
2016-05-30 00:38:02 -0700

    XD :D

--- plugins/ver.lua
+++ plugins/ver.lua
@@ -1,27 +1,20 @@
-do
+local function run(msg)
+local text = [[ [OffliNeTG](http://telegram.me/OffliNeTG)
 
-function run(msg, matches)
-  local text = [[ [OffliNeTG](http://telegram.me/OffliNeTG)
-  
 *Version: 1.1*
-  
-_more information send 
-!OffLiNeTG 
+
+_more information send
+!OffLiNeTG
 [#Dev](http://telegram.me/this_Is_Amir)
 [Join Us :D](http://telegram.me/OffliNeTeam)
 *TNX to all*]]
-send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
 end
 return {
-  description = "Shows bot version", 
-  usage = "!version: Shows bot version",
-  patterns = {
+ patterns = {
     "^[#!/]version$",
     "^[#!/]ver$",
     "^[Vv]ersion$",
     "^[Vv]er$"
-  }, 
-  run = run 
-}
-
-end
+  },
+ run = run }
