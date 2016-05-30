97062807856497f966f90708540d0ee778b000e0
This_Is_Amir <arabiamir13@gmail.com>
2016-05-30 00:45:18 -0700

    XD :D

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,8 +1,7 @@
-do function run(msg, matches)
+local function run(msg)
 if matches[1]:lower() == 'id' and is_momod(msg) then
 local text = "*Your Name*["..msg.from.print_name.."](https://telegram.me/"..msg.from.username..")\n*Your ID* : _"..msg.from.id.."_\n*Group ID* : _"..msg.to.id.."_🔸[OFFLINETEAM](https://telegram.me/offlinetea"
 send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
-end
 end
 return {
 description = "show your id",
@@ -11,6 +10,4 @@ patterns = {
 "^[!/#]([Ii][Dd])$",
 "^[Ii][Dd]$",
 },
-run = run
-}
-end
+ run = run }
